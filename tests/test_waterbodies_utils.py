4cab9ed3bd0458d3499f7b46ac62363d41e12765
purnelldj <davejpurnell@gmail.com>
2024-03-20 18:19:22 -0400

    updated tests

--- tests/test_waterbodies_utils.py
+++ tests/test_waterbodies_utils.py
@@ -1,13 +1,13 @@
 import torch
 
-from datamodules.waterbodies_utils import im_mask_transform
+from datamodules.waterbodies_utils import WbTransform
 
 
 def test_im_mask_transform():
     im = "tests/test_data/im1.jpg"
     mask = "tests/test_data/mask1.jpg"
-    height, width = 512, 512
-    im, mask = im_mask_transform(im, mask, height, width)
+    tfm = WbTransform(512, 512)
+    im, mask = tfm(im, mask)
     assert im.shape == torch.Size([3, 512, 512])
     assert mask.shape == torch.Size([1, 512, 512])
     assert im.dtype == torch.float32
