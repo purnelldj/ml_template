1f5b5ba963db384b99768fc1cb80614451587619
purnelldj <davejpurnell@gmail.com>
2024-04-04 17:46:25 +0000

    vit tfr

--- src/models/vit.py
+++ src/models/vit.py
@@ -36,3 +36,26 @@ class ViTv2(MultiClass):
         logits = F.relu(self.fc1(logits))
         logits = self.fc2(logits)
         return logits
+
+
+class ViTv3(MultiClass):
+    def __init__(self, num_classes: int = 10, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.net = ViTForImageClassification.from_pretrained(
+            "google/vit-base-patch16-224",
+            ignore_mismatched_sizes=True,
+        )
+        in_feats = self.net.classifier.in_features
+        self.net.classifier = nn.Identity()
+        self.net.eval()
+        self.net.requires_grad_(False)
+        self.drop_out = nn.Dropout(0.7)
+        self.fc1 = nn.Linear(in_feats, in_feats)
+        self.fc2 = nn.Linear(in_feats, num_classes)
+
+    def forward(self, x: Tensor) -> Tensor:
+        logits = self.net(x).logits
+        logits = self.drop_out(logits)
+        logits = F.relu(self.fc1(logits))
+        logits = self.fc2(logits)
+        return logits
